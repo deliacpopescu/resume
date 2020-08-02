47200c48d9a7c6018a3b2e20b2cfe1014eacb670
Delia Popescu <deliabuze@gmail.com>
2020-08-02 20:56:52 +0300

    trying to arrange projects page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import "./Projects.css";
+import ReactPlayer from "react-player";
 
 class Projects extends Component {
 
@@ -7,13 +7,34 @@ class Projects extends Component {
     return (
       <div className="d-flex h-100">
         <div className=" container overlay-with-background align-self-center rounded">
-            <h4 className="font-weight-bold font-italic mb-4">Projects</h4>
-            <ul>
-                <li>Hangman</li>
-                <li>The Mover Game</li>
-                <li>Meal's Recipe </li>
-                <li>The Movie Website</li>
-            </ul>
+          <h4 className="font-weight-bold font-italic mb-4">Projects</h4>
+          <div className="mb-5">
+            <div className="row justify-content-center">
+              <div className="mx-auto">
+               <p className="m-0 font-weight-bold font-italic  text-center">Hangman</p>
+               <ReactPlayer
+                url="https://www.dropbox.com/s/cc1hg8by1a7v6mc/hangman.mp4?raw=1" controls={true}  width={250} height={200}/>
+              </div>
+              <div className="mx-auto">
+                <p className="m-0 font-weight-bold font-italic  text-center">The Mover Game</p>
+                <ReactPlayer
+                url="https://www.dropbox.com/s/nklgswz4y51kzit/The%20Mover.mp4?raw=1" controls={true}  width={250} height={200}/>
+              </div>
+            </div>
+            <div className="row mt-5">
+              <div className="mx-auto">
+                <p className="m-0 font-weight-bold font-italic  text-center">Meal's Recipe</p>
+                <ReactPlayer
+                url="https://www.dropbox.com/s/qagy35fiarhvouo/MealsRecipe.mp4?raw=1" controls={true}  width={250} height={200}/>
+              </div>
+              <div className="mx-auto">
+                <p className="m-0 font-weight-bold font-italic text-center">The Movie Website</p>
+                <ReactPlayer
+                url="https://www.dropbox.com/s/zqugbi2meypxs0t/MovieWebsite.mp4?raw=1" controls={true}  width={250} height={200}/>
+              </div>
+            </div>
+
+          </div>
         </div>
       </div>
     )
